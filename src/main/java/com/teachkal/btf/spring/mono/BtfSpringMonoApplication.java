63e3dfbc0ef3eaa5815ee0e54d34386ef41c62f2
TeacH <teach.inbox@gmail.com>
2021-03-19 17:33:34 +0100

    Initial commit

---
+++ src/main/java/com/teachkal/btf/spring/mono/BtfSpringMonoApplication.java
@@ -0,0 +1,13 @@
+package com.teachkal.btf.spring.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtfSpringMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtfSpringMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teachkal/btf/spring/mono/BtfSpringMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachkal.btf.spring.mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtfSpringMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
