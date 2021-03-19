4b554972c3ca5878b021bbd97acf24a965538a13
TeacH <teach.inbox@gmail.com>
2021-03-19 21:07:20 +0100

    mySql properties set up

--- src/main/java/com/teachkal/btf/spring/mono/BtfSpringMonoApplication.java
+++ src/main/java/com/teachkal/btf/spring/mono/BtfSpringMonoApplication.java
@@ -2,8 +2,10 @@ package com.teachkal.btf.spring.mono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BtfSpringMonoApplication {
 
 	public static void main(String[] args) {
