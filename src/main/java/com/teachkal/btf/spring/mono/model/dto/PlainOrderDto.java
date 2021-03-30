530fee6ad30951c2bfb7275786e1fc252ae3de7b
TeacH <teach.inbox@gmail.com>
2021-03-30 23:05:47 +0200

    PlainOrderDto impl

---
+++ src/main/java/com/teachkal/btf/spring/mono/model/dto/PlainOrderDto.java
@@ -0,0 +1,28 @@
+package com.teachkal.btf.spring.mono.model.dto;
+
+import com.teachkal.btf.spring.mono.model.Order;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class PlainOrderDto {
+
+    private Long id;
+    private String uid;
+    private BigDecimal totalPrice;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    public static PlainOrderDto from(Order order) {
+        PlainOrderDto plainOrderDto = new PlainOrderDto();
+        plainOrderDto.setId(order.getId());
+        plainOrderDto.setUid(order.getUid());
+        plainOrderDto.setTotalPrice(order.getTotalPrice());
+        plainOrderDto.setCreateAt(order.getCreatedAt());
+        plainOrderDto.setUpdateAt(order.getUpdatedAt());
+
+        return plainOrderDto;
+    }
+}
