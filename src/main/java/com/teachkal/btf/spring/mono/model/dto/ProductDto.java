426e832823951af22a13607a20ab0e4e6683e9ae
TeacH <teach.inbox@gmail.com>
2021-03-31 16:27:24 +0200

    ProductDto

---
+++ src/main/java/com/teachkal/btf/spring/mono/model/dto/ProductDto.java
@@ -0,0 +1,32 @@
+package com.teachkal.btf.spring.mono.model.dto;
+
+import com.teachkal.btf.spring.mono.model.Product;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class ProductDto {
+    private Long id;
+    private String sku;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ProductDto from(Product product) {
+        ProductDto productDto = new ProductDto();
+
+        productDto.setId(product.getId());
+        productDto.setSku(product.getSku());
+        productDto.setName(product.getName());
+        productDto.setPrice(product.getPrice());
+        productDto.setDescription(product.getDescription());
+        productDto.setCreatedAt(product.getCreatedAt());
+        productDto.setUpdatedAt(product.getUpdatedAt());
+
+        return  productDto;
+    }
+}
