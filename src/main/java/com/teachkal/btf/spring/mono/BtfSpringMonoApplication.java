a5879032df95aa510997fe21879507eb7dc76363
TeacH <teach.inbox@gmail.com>
2021-04-22 16:13:05 +0200

    on context load

--- src/main/java/com/teachkal/btf/spring/mono/BtfSpringMonoApplication.java
+++ src/main/java/com/teachkal/btf/spring/mono/BtfSpringMonoApplication.java
@@ -1,7 +1,10 @@
 package com.teachkal.btf.spring.mono;
 
+import com.teachkal.btf.spring.mono.model.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -10,6 +13,15 @@ public class BtfSpringMonoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BtfSpringMonoApplication.class, args);
+	}
+
+
+	// run after the app context is loaded
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			Logger.info("APP STARTED");
+		};
 	}
 
 }
